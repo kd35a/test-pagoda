949abe8aec84cb8b538ca3c027d46ff37c8791c9
Fredrik Strandin <fredrik.strandin@gmail.com>
2011-08-10 08:28:32 +0200

    Added a start-page

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
+		<title>Test of Pagoda</title>
+	</head>
+
+	<body>
+		<h1>Welcome!</h1>
+		<p>
+			This is a test-page.
+		</p>
+		<p>
+			<?php
+			echo date("y-m-d HH:ii:ss");
+			?>
+		</p>
+	</body>
+</html>
